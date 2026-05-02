78900eca4e9dd8d330ba4e25767c1d3c1e634fb5
Adrian Price <adrianp.quatinus@virginmedia.com>
2026-05-02 16:54:15 +0100

    Web caching strategies for development vs. production
    - web client is now a Java project to support a custom servlet filter class- web-client can now be built for either development or production mode
    - development mode prevents HTML files from being cached by the browser
    - production mode allows HTML caching for two minutes

---
+++ web-client/src/main/java/io/github/demonfiddler/ee/webclient/filter/CacheControlFilter.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------------------------------------------------
+ * Evidence Engine: A system for managing evidence on arbitrary scientific topics.
+ * Comprises an SQL database, GraphQL public API, Java app server, Java and web clients.
+ * Copyright © 2024-26 Adrian Price. All rights reserved.
+ *
+ * This file is part of Evidence Engine.
+ *
+ * Evidence Engine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Evidence Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Evidence Engine.
+ * If not, see <https://www.gnu.org/licenses/>. 
+ *--------------------------------------------------------------------------------------------------------------------*/
+
+package io.github.demonfiddler.ee.webclient.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CacheControlFilter implements Filter {
+
+    private boolean production;
+
+    @Override
+    public void init(FilterConfig config) {
+        String profile = config.getServletContext().getInitParameter("cacheProfile");
+        production = "production".equalsIgnoreCase(profile);
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        if (production) {
+            response.setHeader("Cache-Control", "max-age=120, must-revalidate");
+        } else {
+            response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
+        }
+
+        chain.doFilter(req, res);
+    }
+
+}
